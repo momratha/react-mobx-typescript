e9d24745df90c878d9f5291ffba826feac33c373
ratha <momratha@gmail.com>
2018-12-23 23:20:32 +1100

    add new logic

---
+++ src/index.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {observer} from 'mobx-react';
+import DevTools from 'mobx-react-devtools';
+import AppState from './components/timeViewStore';
+
+
+@observer
+class TimerView extends React.Component<{appState: AppState}, {}> {
+    render() {
+        return (
+            <div>
+                <button onClick={this.onReset}>
+                    Seconds passed: {this.props.appState.timer}
+                </button>
+                <DevTools />
+            </div>
+        );
+     }
+
+     onReset = () => {
+         this.props.appState.resetTimer();
+     }
+};
+
+const appState = new AppState();
+ReactDOM.render(<TimerView appState={appState} />, document.getElementById('root'));
